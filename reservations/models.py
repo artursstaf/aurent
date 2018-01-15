dfc09eb1695108a0301c35a0c463a441b06ca836
Dmitrijs T <dmitrijst@users.noreply.github.com>
2018-01-15 14:02:50 +0200

    Add models

--- reservations/models.py
+++ reservations/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Car(models.Model):
+    brand = models.CharField(max_length=20)
+    name = models.CharField(max_length=10)
+    reg_number = models.CharField(max_length=6)
+    type = models.CharField(max_length=10)
+    year = models.IntegerField(max_length=4)
+
+
+class User(models.Model):
+    name = models.CharField(max_length=20)
+    surname = models.CharField(max_length=20)
+    role = models.CharField(max_length=10, default="employee")
+
+
+class Registration(models.Model):
+    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    start_time = models.DateTimeField
+    end_time = models.DateTimeField
+    notes = models.CharField(max_length=500)
