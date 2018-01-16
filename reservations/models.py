9c9ad755b13c3fe76adcd41693aecf0e76153141
Dmitrijs T <dmitrijst@users.noreply.github.com>
2018-01-16 13:49:52 +0200

    Add user profiles

--- reservations/models.py
+++ reservations/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Car(models.Model):
@@ -9,10 +12,23 @@ class Car(models.Model):
     year = models.IntegerField(verbose_name="Release year")
 
 
-class User(models.Model):
-    name = models.CharField(max_length=40)
-    surname = models.CharField(max_length=40)
-    role = models.CharField(max_length=40, default="employee")
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name="User")
+    name = models.CharField(max_length=40, verbose_name="Name")
+    surname = models.CharField(max_length=40, verbose_name="Surname")
+    phone = models.CharField(max_length=8, verbose_name="Phone", blank=True, null=True)
+    email = models.EmailField(verbose_name="Email", blank=True, null=True)
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
 
 
 class Registration(models.Model):
@@ -27,3 +43,4 @@ class CarCommentary(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="User")
     car = models.ForeignKey(Car, on_delete=models.CASCADE, verbose_name="Car")
     comment = models.CharField(max_length=500)
+    date = models.DateTimeField(verbose_name="Commentary date")
