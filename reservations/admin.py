2403a878f93f5e3c63b596bd35e8ba214ba12261
Artūrs Stafanovičs <arturs.stafanovics@gmail.com>
2018-01-16 14:39:16 +0200

    Registered to admin panel, CarCommentary model, and migrated database.

--- reservations/admin.py
+++ reservations/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Car, Profile, Registration
+from .models import Car, Profile, Registration, CarCommentary
 
 # Register your models here.
 admin.site.register(Car)
 admin.site.register(Profile)
 admin.site.register(Registration)
+admin.site.register(CarCommentary)
