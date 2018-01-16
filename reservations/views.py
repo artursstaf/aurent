664d2866de10615447bb793d9a7a9d5daf4ea7b1
Artūrs Stafanovičs <arturs.stafanovics@gmail.com>
2018-01-16 15:09:49 +0200

    Forgot to add templates in last commit, fixed admin account.

--- reservations/views.py
+++ reservations/views.py
@@ -5,6 +5,7 @@ from reservations.models import Registration
 
 
 def show_registrations(request):
+
     all_registrations = Registration.objects.all().order_by('start_time')
     context = {'all_registrations': all_registrations}
     return render(request, 'reservations/view_registrations.html', context)
